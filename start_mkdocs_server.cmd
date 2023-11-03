a40256c41644d53d2d0b5c879bca12897d74b54b
Burggraf <burggraf>
2023-11-03 08:13:51 +0100

    initial commit

---
+++ start_mkdocs_server.cmd
@@ -0,0 +1,5 @@
+pip --proxy kjs-03.lan.dd-schulen.de:3128 install mkdocs mkdocs-material
+pip --proxy kjs-03.lan.dd-schulen.de:3128 install mkdocs python-markdown-math
+cd /d "J:\PortableApps\mkdocs_lf8_vorlage"
+c:\users\burggraf\appdata\roaming\python\python310\scripts\mkdocs serve
+pause
